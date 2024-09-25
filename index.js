87099e746e9baf38c4797ac39e3deaf35d661898
Amit Kumar Mahto <amit.mahto3097@gmail.com>
2024-09-25 20:35:24 +0530

    updated file

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Middleware for parsing JSON bodies
-app.use(bodyParser.json()); // for JSON payloads
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); // for URL-encoded payloads
 
-const db = require('./config/mongoose'); // Ensure MongoDB connection is properly established
+const db = require('./config/mongoose'); 
 
 // Routes
 app.use('/', require('./routes/index'));
